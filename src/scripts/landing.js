52296b89396f1b28fb0b6832245a6a430e7375f2
dovigod <inaki96@naver.com>
2022-04-11 17:53:29 +0900

    logo , favicon add

--- src/scripts/landing.js
+++ src/scripts/landing.js
@@ -1,14 +1,10 @@
 import * as PIXI from 'pixi.js';
 import { Point } from '@pixi/math';
 
-const canvas = document.getElementById('canvas')
+const canvas = document.getElementById('canvas');
 
 const circleSvg = document.getElementsByClassName('helper_circle')[0];
 const helperPath = document.getElementsByClassName('helper_path')[0];
-
-
-
-
 
 const screenSize = {
 	width: window.innerWidth,
@@ -22,8 +18,8 @@ unCover.beginFill(0x000000);
 unCover.drawCircle(0, 0, 100);
 unCover.endFill();
 const app = new PIXI.Application({
-	width: window.innerWidth*2 / 3,
-	height: window.innerHeight ,
+	width: (window.innerWidth * 2) / 3,
+	height: window.innerHeight,
 	resolution: window.devicePixelRatio,
 	autoDensity: true,
 	view: canvas
@@ -39,8 +35,6 @@ app.loader
 		setup();
 	});
 
-
-
 const setup = () => {
 	const brushTexture = app.loader.resources.bristle1.texture;
 
@@ -48,7 +42,7 @@ const setup = () => {
 
 	brush.width = brushWidth;
 	brush.height = brushHeight;
-	brush.anchor.set(0.5,0.5);
+	brush.anchor.set(0.5, 0.5);
 
 	const backgroundTexture = app.loader.resources.background.texture;
 
@@ -59,7 +53,6 @@ const setup = () => {
 	background.anchor.x = 0.5;
 	background.anchor.y = 0.5;
 
-
 	const mask = new PIXI.Sprite(PIXI.Texture.WHITE);
 	mask.width = app.renderer.screen.width;
 	mask.height = app.renderer.screen.height;
@@ -67,7 +60,6 @@ const setup = () => {
 	mask.y = app.renderer.screen.height / 2;
 	mask.anchor.x = 0.5;
 	mask.anchor.y = 0.5;
-
 
 	app.stage.addChild(mask);
 	app.stage.addChild(background);
@@ -91,46 +83,39 @@ const setup = () => {
 	const originVector = new Point(1, 0);
 	let vector;
 
-
-
 	const positionHistory = {
-		start : {
-			x : null,
-			y : null,
+		start: {
+			x: null,
+			y: null
 		},
-		end : {
-			x : null,
-			y : null
+		end: {
+			x: null,
+			y: null
 		}
-	}
+	};
 	function pointerMove(event) {
 		if (dragging) {
 			brush.position.copyFrom(event.data.global);
 
 			brush.width += 0.1;
 			const originPos = {
-				x : event.data.global.x - window.innerWidth / 2,
-				y : -(event.data.global.y - window.innerHeight / 2)
-			}
+				x: event.data.global.x - window.innerWidth / 2,
+				y: -(event.data.global.y - window.innerHeight / 2)
+			};
 
-	
-			
 			const vx = originPos.x;
 			const vy = originPos.y;
-			
+
 			vector = new Point(vx, vy);
 			vector = normalizeVector(vector);
 
-			const dotProd = (vector.x * originVector.x) + (vector.y * originVector.y);
-	
+			const dotProd = vector.x * originVector.x + vector.y * originVector.y;
+
 			const angle = Math.acos(dotProd);
-			const rotationMatrix = new PIXI.Matrix()
+			const rotationMatrix = new PIXI.Matrix();
 			rotationMatrix.rotate(angle);
 			brush.rotation = angle;
 
-
-
-			
 			app.renderer.render(
 				brush,
 				{
@@ -141,10 +126,8 @@ const setup = () => {
 				null,
 				false
 			);
-
-		}
-		else{
-			if(startErase){
+		} else {
+			if (startErase) {
 				unCover.position.copyFrom(event.data.global);
 
 				app.renderer.render(
@@ -159,70 +142,61 @@ const setup = () => {
 				);
 			}
 		}
-		if(endPos.x -50 <= event.data.global.x && (endPos.y + 50 >= event.data.global.y || endPos.y -50 <= event.data.global.y) && !isHit && dragging){
+		if (
+			endPos.x - 50 <= event.data.global.x &&
+			(endPos.y + 50 >= event.data.global.y || endPos.y - 50 <= event.data.global.y) &&
+			!isHit &&
+			dragging
+		) {
 			isHit = true;
 			// window.removeEventListener('mousemove' , exitLanding)
 			const target = document.head.childNodes[3];
-			
 
-
-	
 			const xhr = new XMLHttpRequest();
 			const section = document.getElementsByClassName('landing')[0];
 
-		
-			document.body.children[1].removeChild(helperPath)
+			document.body.children[2].removeChild(helperPath);
 			// document.head.removeChild(target);
-		
-	
+
 			xhr.onload = () => {
-	
-				if(xhr.status === 200){
+				if (xhr.status === 200) {
 					document.body.innerHTML += xhr.responseText;
-					document.body.children[2].classList.add('load')
-				}else{
+					document.body.children[3].classList.add('load');
+				} else {
 					console.warn('erroor');
 				}
-			}
-			xhr.open('get' , './partial.html');
+			};
+			xhr.open('get', './partial.html');
 			xhr.send();
-	
 		}
 	}
 
-	function pointerDown(event , x ,y) {
+	function pointerDown(event, x, y) {
 		dragging = true;
 		startErase = false;
 		positionHistory.start.x = event.data.global.x ? event.data.global.x : x;
-		positionHistory.start.y = event.data.global.y ? event.data.global.y: y;
-		helperPath.classList.add('show_up')
-		
+		positionHistory.start.y = event.data.global.y ? event.data.global.y : y;
+		helperPath.classList.add('show_up');
+
 		pointerMove(event);
 	}
 
 	function pointerUp(event) {
-
 		dragging = false;
 		brush.width = brushWidth;
-		console.dir(app)
+		console.dir(app);
 		positionHistory.end.x = event.data.global.x;
 		positionHistory.end.y = event.data.global.y;
-
 
 		positionHistory.start.x = null;
 		positionHistory.start.y = null;
 		positionHistory.end.x = null;
 		positionHistory.end.y = null;
-		
-	
 	}
 
-window.addEventListener('click' , (e) => {
-	
-	pointerDown(null , e.x ,e.y);
-})
-
-
+	window.addEventListener('click', (e) => {
+		pointerDown(null, e.x, e.y);
+	});
 
 	window.addEventListener('resize', () => {
 		screenSize.width = window.innerWidth;
@@ -232,34 +206,31 @@ window.addEventListener('click' , (e) => {
 		app.renderer.stage.width = window.innerWidth;
 		app.renderer.stage.height = window.innerHeight;
 	});
-
 };
 
+const getMagnitude = (x, y) => {
+	return Math.sqrt(x * x + y * y);
+};
 
-
-const getMagnitude = (x , y) => {
-	return Math.sqrt(x*x + y*y);
-}
-
-const normalizeVector = (vector) =>{
-	const magnitude = getMagnitude(vector.x , vector.y);
+const normalizeVector = (vector) => {
+	const magnitude = getMagnitude(vector.x, vector.y);
 	vector.x /= magnitude;
 	vector.y /= magnitude;
 
 	return vector;
-}
-
+};
 
 //// terminator
 
 let isHit = false;
 const endPos = {
-	x : window.innerWidth /6 + window.innerWidth*1/2,
-	y : window.innerHeight / 2
-}
-circleSvg.addEventListener('click' , _ => {
-	
+	x: window.innerWidth / 6 + (window.innerWidth * 1) / 2,
+	y: window.innerHeight / 2
+};
+circleSvg.addEventListener('click', (_) => {
+	console.dir(document.body);
 	helperPath.classList.add('show_up');
-	document.body.children[1].children[2].remove()
-	document.body.children[1].children[1].remove()
-})
+
+	document.body.children[2].children[2].remove();
+	document.body.children[2].children[1].remove();
+});
